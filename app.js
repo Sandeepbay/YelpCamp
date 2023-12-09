0300aba2870bd8eb0c2f6d8b3f46a25572375f02
sandeepbay2002@gmail.com <sandeepbay2002@gmail.com>
2023-12-09 19:52:58 +0530

    Added Styling
    Added Some Styling , using ejs-mate package - Added Footer and Partial to the project.

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const path = require("path");
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
 const Campground = require("./models/campground");
 const campground = require("./models/campground");
 
@@ -15,8 +16,9 @@ db.on("open", () => {
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}))
-app.use(methodOverride("_method"))
+app.engine("ejs", ejsMate);
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -25,42 +27,44 @@ app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/campgrounds" , async (req,res) => {
-    const campgrounds = await Campground.find()
-    res.render("campground/index" , {campgrounds})
-})
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find();
+  res.render("campground/index", { campgrounds });
+});
 
-app.get("/campgrounds/new" , (req,res) => {
-    res.render("campground/new")
-})
+app.get("/campgrounds/new", (req, res) => {
+  res.render("campground/new");
+});
 
-app.post("/campgrounds" , async (req,res) => {
-    const campground = new Campground(req.body.campground)
-    await campground.save()
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+app.post("/campgrounds", async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
 
-app.get("/campgrounds/:id" , async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render("campground/show" , {campground})
-})
+app.get("/campgrounds/:id", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campground/show", { campground });
+});
 
-app.get("/campgrounds/:id/edit" , async (req,res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render("campground/edit" , {campground})
-})
+app.get("/campgrounds/:id/edit", async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render("campground/edit", { campground });
+});
 
-app.put("/campgrounds/:id" , async (req,res) => {
-    const {id} = req.params
-    const campground  = await Campground.findByIdAndUpdate(id , {...req.body.campground})
-    res.redirect(`/campgrounds/${campground._id}`)
-})
+app.put("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
+  res.redirect(`/campgrounds/${campground._id}`);
+});
 
-app.delete("/campgrounds/:id" , async (req,res) => {
-    const {id} = req.params
-    await Campground.findByIdAndDelete(id)
-    res.redirect("/campgrounds")
-})
+app.delete("/campgrounds/:id", async (req, res) => {
+  const { id } = req.params;
+  await Campground.findByIdAndDelete(id);
+  res.redirect("/campgrounds");
+});
 
 app.listen(3000, () => {
   console.log("Server running on Port 3000");
